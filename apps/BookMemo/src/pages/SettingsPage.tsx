d3bf73a991262ad900cf931a423076031a9ab302
Seyeon Weon <seyeon.weon@gmail.com>
2026-02-12 08:34:03 +0900

    fix: remove unused useEffect import in BookMemo settings

--- apps/BookMemo/src/pages/SettingsPage.tsx
+++ apps/BookMemo/src/pages/SettingsPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { AppLockSettings, DataManagementSection, LanguageSettings, ThemeSettings, useColorTheme, useConfirm, useLanguage } from '@memosuite/shared';
 
 import styled from 'styled-components';
